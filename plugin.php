b26475bd466edafc3fe3972abf8a38e18d90e287
theodortruffer <tt@studer-raimann.ch>
2015-09-10 15:18:19 +0200

    updated ilias-/plugin-version

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'sys_not';
-$version = '1.2.1';
+$version = '1.3.0';
 $ilias_min_version = '4.4.0';
-$ilias_max_version = '5.0.999';
+$ilias_max_version = '5.1.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
 ?>
