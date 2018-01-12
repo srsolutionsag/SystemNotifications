a7c7b6296ac0acb6836b241f55c640443b5233c8
Florian <fw@studer-raimann.ch>
2018-01-12 08:12:35 +0100

    Remove ILIAS 5.1 support

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'sys_not';
 $version = '1.6.0';
-$ilias_min_version = '5.1.0';
+$ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'support@studer-raimann.ch';
