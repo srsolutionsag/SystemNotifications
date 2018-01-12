ed42a92825e4ab64026abba1878ace251089df34
Florian <fw@studer-raimann.ch>
2018-01-12 08:10:08 +0100

    ILIAS 5.3 support

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'sys_not';
 $version = '1.6.0';
 $ilias_min_version = '5.1.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.3.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'support@studer-raimann.ch';
