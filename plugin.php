318e13f4ba90f32478fcfebbcbae2313a0b79875
chfsx <fs@studer-raimann.ch>
2021-04-08 09:23:52 +0200

    updated plugin version

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id                = 'sys_not';
-$version           = '2.0.0';
+$version           = '2.0.1';
 $ilias_min_version = '6.000';
 $ilias_max_version = '6.999';
 $responsible       = 'studer + raimann ag';
